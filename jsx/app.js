b57e2e3bc44fe690a11cb5d09bdcd8e23ab0b2ec
n-fukidome <n-fukidome@nekojarashi.com>
2015-04-06 08:17:44 +0900

    use variable

--- jsx/app.js
+++ jsx/app.js
@@ -1,3 +1,15 @@
+// React.render(
+//   <h1>Hello, world!</h1>, document.body
+// );
+
+var Hello = React.createClass({
+  render: function() {
+    return (
+      <div className="container">Hello {this.props.name}</div>
+    );
+  }
+})
+
 React.render(
-  <h1>Hello, world!</h1>, document.body
+  <Hello name="React" />, document.body
 );
