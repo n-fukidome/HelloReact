e9415264dc253d070fd7b2863e3da23191f5765a
n-fukidome <n-fukidome@nekojarashi.com>
2015-04-06 09:07:59 +0900

    getDefaultProps

--- jsx/app.js
+++ jsx/app.js
@@ -3,13 +3,16 @@
 // );
 
 var Hello = React.createClass({
-  render: function() {
-    return (
-      <div className="container">Hello {this.props.name}</div>
-    );
+  getDefaultProps() {
+    return{
+      name: 'React'
+    };
+  },
+  render() {
+    return <div className="container">Hello {this.props.name}</div>
   }
-})
+});
 
 React.render(
-  <Hello name="React" />, document.body
+  <Hello />, document.body
 );
